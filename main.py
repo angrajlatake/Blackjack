f089f5e4816116da0b0048fb2c07652019aa0739
angrajlatake <angraj.latake@gmail.com>
2021-08-12 17:37:39 +0530

    code is finished. need to add comments

--- main.py
+++ main.py
@@ -4,36 +4,31 @@ cards = ["K", "Q", "J", 9, 8, 7, 6, 5, 4, 3, 2, "A"]
 
 users_cards = []
 dealers_cards = []
-def Deal_cards(bet):
+def Deal_cards():
     for i in range(2):
         users_cards.append(random.choice(cards))
         dealers_cards.append(random.choice(cards))
-    print(f"Your bet is ${bet}")
-    print(f"Your cards: {users_cards}")
-    print(f"Dealer's cards: [{dealers_cards[0]}, X]")
-
+    print(f"Your cards: {users_cards}. Your score is : {Check_total(users_cards)} \nDealer's cards: [{dealers_cards[0]}, X]")
+    print(f"")
 def Check_total(cards):
     total = 0
-    if "A" in cards:
+    for card in cards:
+        if card == "K" or card =="J" or card == "Q":
+            card = 10
+        elif card == "A":
+            card = 11
+        total += card
+    if total > 21:
+        total = 0
         for card in cards:
             if card == "K" or card =="J" or card == "Q":
-                total += 10
+                card = 10
             elif card == "A":
-                total += 11
-            total += card
-        if total > 21:
-            total = 0 
-            for card in cards:
-            if card == "K" or card =="J" or card == "Q":
-                total += 10
-            elif card == "A":
-                total += 1
+                card = 1
             total += card
         return total
-    else:
-        for card in cards:
-            total += card
-        return total
+
+    return total
 
 def add_card(list):
     list.append(random.choice(cards))
@@ -41,18 +36,32 @@ def add_card(list):
 start = input("Do you want to play BlackJack. y or n?")
 #If yes, then clear the screen and start the game
 if start == "y":
-    bet = int(input("How much do you want to bet?"))
-    Deal_cards(bet)
+    Deal_cards()
 #ask for the bet amount
-
+user_lost = False
 continue_game = True
 while continue_game:
     hit = input('Do you want another card. y or n?')
     if hit == "y":
         add_card(users_cards)
-        Check_total(users_cards)
+        print(users_cards)
+        print(f"Your score is :{Check_total(users_cards)}")
+        if Check_total(users_cards) > 21:
+            continue_game = False
+            print("You went bust")
+            user_lost = True
+    else:
+        continue_game = False
+if not user_lost:
+    print(f"Dealer's cards: {dealers_cards}")
+    while Check_total(dealers_cards) < 17:
+        add_card(dealers_cards)
+        print(f"Drawing card. \n Dealer's Cards : {dealers_cards}")
 
+    if Check_total(users_cards) > Check_total(dealers_cards):
+        print(f"Your score is {Check_total(users_cards)} and dealer is at {Check_total(dealers_cards)}. You won!")
+    elif Check_total(users_cards) == Check_total(dealers_cards):
+        print("Game tied")
+    else:
+        print(f"Your score is {Check_total(users_cards)} and dealer is at {Check_total(dealers_cards)}. Dealer won!")
 
-#Select two random cards for the user and one card for comp
-# ask the user if user wants another card or let it pass
-#Calculate the 
